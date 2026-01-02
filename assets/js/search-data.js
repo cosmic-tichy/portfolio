f16fd488628096220d9891704a45d955d3cfadbb
cosmic-tichy <36252522+cosmic-tichy@users.noreply.github.com>
2026-01-02 22:21:49 +0000

    Deploying to gh-pages from @ cosmic-tichy/portfolio@b30890b7072124fc008321c0f9cce122ae3539b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,30 +447,4 @@ ninja.data = [{
         handler: () => {
           window.open("mailto:%6E%69%63%6B%6D%6F%77%65%72%79%6D%75%73%69%63@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
